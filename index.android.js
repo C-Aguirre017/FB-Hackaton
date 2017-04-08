3a159d5cda6eaead9a1e94aa810755a71e8eca89
Enrique Correa <quiquecorreav@gmail.com>
2017-04-08 16:10:36 -0300

    diseno inicial

--- index.android.js
+++ index.android.js
@@ -6,51 +6,50 @@
 
 import React, { Component } from 'react';
 import Share from './share';
+import Modal from 'react-native-modalbox'
+import Spinner from 'react-native-spinkit';
 
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Image
 } from 'react-native';
 
 export default class boiler extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      isOpen: true,
+      value: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{flex: 1}}>
+        <View style={{ borderColor: 'green', borderWidth: 1, backgroundColor: 'white', margin: 10, borderRadius: 2, padding: 5, flexDirection: 'column'}}>
+          <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              source={require('./images/eye.png')}
+              style={{width: 50, height: 50}}
+            />
+            <Text style={{marginLeft: 10, fontSize: 30, fontWeight: 'bold', color: '#77B3D4'}}>PAPIRO</Text>
+          </View>
+          <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <Spinner isVisible={true} size={100} type={'Wave'} color={'#77B3D4'}/>
+          </View>
+          <View>
+            <ScrollView>
+              <Text style={{fontSize: 20, color: '#000000', textAlign: 'justify'}}>{ this.state.value }</Text>
+            </ScrollView>
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('boiler', () => boiler);
 AppRegistry.registerComponent('MyShareEx', () => Share)
--- share.js
+++ share.js
@@ -51,7 +51,7 @@ export default class Share extends Component {
       <Modal backdrop={false}
              style={{ backgroundColor: 'transparent' }} position="center" isOpen={this.state.isOpen} onClosed={this.onClose}>
           <View style={{ alignItems: 'center', justifyContent:'center', flex: 1 }}>
-            <View style={{ borderColor: 'green', borderWidth: 1, backgroundColor: 'white', height: 200, width: 300 }}>
+            <View style={{ borderWidth: 0, backgroundColor: 'white', margin: 10, borderRadius: 2, padding: 5}}>
               <TouchableOpacity onPress={this.closing}>
                 <Text>Close</Text>
                 <Text>type: { this.state.type }</Text>
