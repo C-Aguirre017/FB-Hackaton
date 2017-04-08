3096b8fa8ca2fcfa0fa301a058aecd6fd9e0bf95
Enrique Correa <quiquecorreav@gmail.com>
2017-04-08 14:46:46 -0300

    share funcionando

--- share.js
+++ share.js
@@ -1,50 +1,65 @@
+/**
+ * Sample React Native Share Extension
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import Modal from 'react-native-modalbox'
 import ShareExtension from 'react-native-share-extension'
 
-import React, { Component } from 'react';
-
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
-  View
-} from 'react-native';
+  TextInput,
+  View,
+  TouchableOpacity
+} from 'react-native'
 
-export default class boiler extends Component {
+export default class Share extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      isOpen: true,
+      type: '',
+      value: ''
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { type, value } = await ShareExtension.data()
+      this.setState({
+        type,
+        value
+      })
+    } catch(e) {
+      console.log('errrr', e)
+    }
+  }
+
+  onClose() {
+    ShareExtension.close()
+  }
+
+  closing = () => {
+    this.setState({
+      isOpen: false
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      <Modal backdrop={false}
+             style={{ backgroundColor: 'transparent' }} position="center" isOpen={this.state.isOpen} onClosed={this.onClose}>
+          <View style={{ alignItems: 'center', justifyContent:'center', flex: 1 }}>
+            <View style={{ borderColor: 'green', borderWidth: 1, backgroundColor: 'white', height: 200, width: 300 }}>
+              <TouchableOpacity onPress={this.closing}>
+                <Text>Close</Text>
+                <Text>type: { this.state.type }</Text>
+                <Text>value: { this.state.value }</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+      </Modal>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-const { type, value } = await ShareExtension.data()
